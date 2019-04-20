ad22114ccd4389daa471900afda95e1148c2b467
Boris Mukovnin <ninwim@gmail.com>
2019-04-20 03:06:18 +0300

    Добавил временную зону и даты в массив постов

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('Europe/Moscow');
+
 require_once 'helpers.php';
 require_once 'functions.php';
 
@@ -13,42 +15,48 @@ $popular_posts = [
         'type' => 'post-quote',
         'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
         'author' => 'Лариса',
-        'avatar' => 'userpic-larisa-small.jpg'
+        'avatar' => 'userpic-larisa-small.jpg',
+        'datetime' => generate_random_date(mt_rand(0,4))
     ],
     [
         'subject' => 'Игра пристолов',
         'type' => 'post-text',
         'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
         'author' => 'Владик',
-        'avatar' => 'userpic.jpg'
+        'avatar' => 'userpic.jpg',
+        'datetime' => generate_random_date(mt_rand(0,4))
     ],
     [
         'subject' => 'Наконец, обработал фотки!',
         'type' => 'post-photo',
         'content' => 'rock-medium.jpg',
         'author' => 'Виктор',
-        'avatar' => 'userpic-mark.jpg'
+        'avatar' => 'userpic-mark.jpg',
+        'datetime' => generate_random_date(mt_rand(0,4))
     ],
     [
         'subject' => 'Моя мечта',
         'type' => 'post-photo',
         'content' => 'coast-medium.jpg',
         'author' => 'Лариса',
-        'avatar' => 'userpic-larisa-small.jpg'
+        'avatar' => 'userpic-larisa-small.jpg',
+        'datetime' => generate_random_date(mt_rand(0,4))
     ],
     [
         'subject' => 'Лучшие курсы',
         'type' => 'post-link',
         'content' => 'www.htmlacademy.ru',
         'author' => 'Владик',
-        'avatar' => 'userpic.jpg'
+        'avatar' => 'userpic.jpg',
+        'datetime' => generate_random_date(mt_rand(0,4))
     ],
     [
         'subject' => 'Полезный пост про Байкал',
         'type' => 'post-text',
         'content' => 'Озеро Байкал – огромное древнее озеро в горах Сибири к северу от монгольской границы. Байкал считается самым глубоким озером в мире. Он окружен сетью пешеходных маршрутов, называемых Большой байкальской тропой. Деревня Листвянка, расположенная на западном берегу озера, – популярная отправная точка для летних экскурсий. Зимой здесь можно кататься на коньках и собачьих упряжках.',
         'author' => 'Лариса',
-        'avatar' => 'userpic-larisa-small.jpg'
+        'avatar' => 'userpic-larisa-small.jpg',
+        'datetime' => generate_random_date(mt_rand(0,4))
     ],
 ];
 
